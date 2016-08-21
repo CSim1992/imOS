80d7d5f0166ebb4d4430a380873c5b89a1514960
CSim1992 <1416776996@qq.com>
2016-08-21 21:03:57 +0800

    API请求数据

--- app/QueryPhone.php
+++ app/QueryPhone.php
@@ -1,11 +1,13 @@
 <?php
 
 	namespace app;
+	use libs\ImHttpRequest;
 	class QueryPhone{
 		const TAOBAO_API = 'https://tcc.taobao.com/cc/json/mobile_tel_segment.htm';
 		public static function query($phone){
 			if(self::verifyPhone($phone)){
-				
+				$response = ImHttpRequest::request(self::TAOBAO_API,['tel'=>$phone]);
+				var_dump($response);
 			}
 		}
 		//校验手机号码合法性
